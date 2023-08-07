af0d1e4a13e7a010bb78a9e55cb963ed361daae8
dat-nguyen-304 <nguyenphatdat3004@gmail.com>
2023-08-08 02:19:36 +0700

    fix navbar flash

--- components/navbar/Search.tsx
+++ components/navbar/Search.tsx
@@ -1,7 +1,8 @@
-import useUser from "@/hooks/useUser";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import useUser from '@/hooks/useUser';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { Navbar } from 'flowbite-react';
+import Link from 'next/link';
 
 const Search = () => {
     const myUser = useUser();
@@ -14,82 +15,84 @@ const Search = () => {
     useEffect(() => {
         if (myUser.user) {
             setRole(myUser.user.role);
-        } else setRole("guest");
+        } else setRole('guest');
     }, [myUser.user]);
 
-    if (role === "company") menu = [
-        {
-            name: "Trang chủ",
-            path: "/"
-        },
-        {
-            name: "Kols",
-            path: "/kols"
-        }, {
-            name: "Đăng chiến dịch mới",
-            path: "/campaigns/post"
-        }, {
-            name: "Chiến dịch của tôi",
-            path: "/campaigns/my-campaigns"
-        }, {
-            name: "Đăng ký gói",
-            path: "/payment"
-        }
-    ];
-    else if (role === "kol") menu = [
-        {
-            name: "Trang chủ",
-            path: "/"
-        },
-        {
-            name: "Chiến dịch",
-            path: "/campaigns"
-        }, {
-            name: "Đăng ký gói",
-            path: "/payment"
-        }
-    ];
-    else menu = [
-        {
-            name: "Trang chủ",
-            path: "/"
-        },
-        {
-            name: "Kols",
-            path: "/kols"
-        },
-        {
-            name: "Chiến dịch",
-            path: "/campaigns"
-        }
-    ];
+    if (role === 'company')
+        menu = [
+            {
+                name: 'Trang chủ',
+                path: '/'
+            },
+            {
+                name: 'Kols',
+                path: '/kols'
+            },
+            {
+                name: 'Đăng chiến dịch mới',
+                path: '/campaigns/post'
+            },
+            {
+                name: 'Chiến dịch của tôi',
+                path: '/campaigns/my-campaigns'
+            },
+            {
+                name: 'Đăng ký gói',
+                path: '/payment'
+            }
+        ];
+    else if (role === 'kol')
+        menu = [
+            {
+                name: 'Trang chủ',
+                path: '/'
+            },
+            {
+                name: 'Chiến dịch',
+                path: '/campaigns'
+            },
+            {
+                name: 'Đăng ký gói',
+                path: '/payment'
+            }
+        ];
+    else
+        menu = [
+            {
+                name: 'Trang chủ',
+                path: '/'
+            },
+            {
+                name: 'Kols',
+                path: '/kols'
+            },
+            {
+                name: 'Chiến dịch',
+                path: '/campaigns'
+            }
+        ];
 
     return (
         <div className="flex-1 md:flex-none">
-            <Navbar
-                fluid
-                rounded
-                className="!bg-green-100"
-            >
+            <Navbar fluid rounded className="!bg-green-100">
                 <Navbar.Toggle />
                 <Navbar.Collapse>
-                    {
-                        menu.map((item, index) => (
-                            <Navbar.Link key={ index }
-                                active={ path === item.path ? true : false }
-                                href={ item.path }
-                                className={ path === item.path ? "bg-green-200 !text-green-500" : "" }
-                            >
-                                <p className="text-sm md:text-base">
-                                    { item.name }
-                                </p>
-                            </Navbar.Link>
-                        ))
-                    }
+                    {menu.map((item, index) => (
+                        <Navbar.Link
+                            key={index}
+                            active={path === item.path ? true : false}
+                            as={'div'}
+                            className={path === item.path ? 'bg-green-200 !text-green-500' : ''}
+                        >
+                            <Link href={item.path}>
+                                <p className="text-sm md:text-base">{item.name}</p>
+                            </Link>
+                        </Navbar.Link>
+                    ))}
                 </Navbar.Collapse>
             </Navbar>
         </div>
     );
-}
+};
 
-export default Search
+export default Search;
--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -1,17 +1,17 @@
-import { AiOutlineMenu } from "react-icons/ai";
-import Avatar from "../Avatar";
-import { useCallback, useState } from "react";
-import MenuItem from "./MenuItem";
-import useRegisterModal from "../../hooks/useRegisterModal";
-import useLoginModal from "../../hooks/useLoginModal";
-import { useRouter } from "next/navigation";
-import { Company, Kol, User, UserRole } from "@/types";
-import useUser from "@/hooks/useUser";
-import useKol from "@/hooks/useKol";
-import useCompany from "@/hooks/useCompany";
+import { AiOutlineMenu } from 'react-icons/ai';
+import Avatar from '../Avatar';
+import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
+import useRegisterModal from '../../hooks/useRegisterModal';
+import useLoginModal from '../../hooks/useLoginModal';
+import { useRouter } from 'next/navigation';
+import { Company, Kol, User, UserRole } from '@/types';
+import useUser from '@/hooks/useUser';
+import useKol from '@/hooks/useKol';
+import useCompany from '@/hooks/useCompany';
 
 interface UserMenuProps {
-    currentUser?: Kol | Company | null
+    currentUser?: Kol | Company | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
@@ -20,7 +20,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
     const toggleOpen = useCallback(() => {
-        setIsOpen((value) => !value)
+        setIsOpen(value => !value);
     }, []);
     const myUser = useUser();
     const myKol = useKol();
@@ -31,63 +31,63 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         if (myUser.user?.role === UserRole.company) {
             myCompany.onChangeCompany(null);
         } else myKol.onChangeKol(null);
-        localStorage.removeItem("6live_email");
-        localStorage.removeItem("6live_role");
-        router.push("/");
-    }
+        localStorage.removeItem('6live_email');
+        localStorage.removeItem('6live_role');
+        router.push('/');
+    };
 
     return (
-        <div className='relative'>
-            <div className='flex flex-row items-center gap-3'>
-                <div onClick={ toggleOpen } className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'>
+        <div className="relative">
+            <div className="flex flex-row items-center gap-3">
+                <div
+                    onClick={toggleOpen}
+                    className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+                >
                     <AiOutlineMenu />
                     <div className="block">
-                        <Avatar src={ currentUser?.image } />
+                        <Avatar src={currentUser?.image} />
                     </div>
                 </div>
             </div>
-            { isOpen && (
+            {isOpen && (
                 <div className="absolute z-[100] rounded-xl shadow-mdw-[40vw]md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
                     <div className="flex flex-col cursor-pointer z-[10]">
-                        { currentUser ? (
+                        {currentUser ? (
                             <div className="z-10 bg-slate-50 shadow-lg">
                                 <MenuItem
                                     label="Hồ sơ"
-                                    onClick={ () => { router.push("/profile") } }
+                                    onClick={() => {
+                                        router.push('/profile');
+                                    }}
                                 />
                                 <hr />
                                 <MenuItem
                                     label="Lịch sử giao dịch"
-                                    onClick={ () => { router.push("/history") } }
+                                    onClick={() => {
+                                        router.push('/history');
+                                    }}
                                 />
                                 <hr />
                                 <MenuItem
                                     label="Đổi mật khẩu"
-                                    onClick={ () => { router.push("/change-password") } }
+                                    onClick={() => {
+                                        router.push('/change-password');
+                                    }}
                                 />
                                 <hr />
-                                <MenuItem
-                                    label="Đăng xuất"
-                                    onClick={ handleLogout }
-                                />
+                                <MenuItem label="Đăng xuất" onClick={handleLogout} />
                             </div>
                         ) : (
                             <>
-                                <MenuItem
-                                    label="Đăng nhập"
-                                    onClick={ loginModal.onOpen }
-                                />
-                                <MenuItem
-                                    label="Đăng ký"
-                                    onClick={ registerModal.onOpen }
-                                />
+                                <MenuItem label="Đăng nhập" onClick={loginModal.onOpen} />
+                                <MenuItem label="Đăng ký" onClick={registerModal.onOpen} />
                             </>
-                        ) }
+                        )}
                     </div>
                 </div>
-            ) }
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default UserMenu
+export default UserMenu;
